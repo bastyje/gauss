e748452551e77fe8c8ccfa0517366367a28c150f
Jakub Maciejewski <jakubm55555@gmail.com>
2020-12-28 15:23:47 +0100

    Added implementation of back substituion in function backsubst in file backsubst.c

--- trunk/src/backsubst.c
+++ trunk/src/backsubst.c
@@ -4,19 +4,31 @@
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
-int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
+int backsubst(Matrix *x, Matrix *mat, Matrix *b)
+{
+	/*
+	* Tutaj należy umieścić właściwą implemntację.
+	*/
+	int i, j;
+	for (i = x->r - 1; i >= 0; i--)
+	{
+		x->data[i][0] = b->data[i][0];
+		for (j = x->r - 1; j > i; j--)
+		{
+			x->data[i][0] = x->data[i][0] - mat->data[i][j] * x->data[j][0];
+		}
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
+		x->data[i][0] = x->data[i][0] / mat->data[i][i];
+	}
 
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
+	/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
 
-				return 0;
+	/*
+	for (i = 0; i < x->r; i++)
+	{
+		x->data[i][0] = b->data[i][0];
+	}
+	*/
+
+	return 0;
 }
-
-
