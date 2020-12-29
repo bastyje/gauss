168bb22b300050a846438d70fee0e6ba3b59b87c
bastyje <sebastiangorka442@gmail.com>
2020-12-29 14:33:41 +0100

    gauss.h: added prototypes of functions mainElem and determinant; gauss.c: added function determinant and used it in function eliminate

--- trunk/src/gauss.h
+++ trunk/src/gauss.h
@@ -7,6 +7,9 @@
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
-int eliminate(Matrix *mat, Matrix *b);
+int eliminate(Matrix *A, Matrix *b);
 
+void mainElem(Matrix *A, Matrix *b, int r, int c);
+
+double determinant(Matrix *A, int n);
 #endif
